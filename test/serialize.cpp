cf7e55921390ee9c4bcee9200edb21a472d7aeec
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-01-14 17:35:47 +0100

    Added initial test for the new load and save functionality

--- test/serialize.cpp
+++ test/serialize.cpp
@@ -5,25 +5,79 @@
 
 using namespace MultivariateSplines;
 
-int main() {
-    DataTable t;
-    t.addSample(2.0, 1.0);
+// Checks if a is within margin of b
+bool equalsWithinRange(double a, double b, double margin = 0.0)
+{
+    return b - margin <= a && a <= b + margin;
+}
 
-    std::cout << get_size(t) << std::endl;
+bool is_identical(DataTable &a, DataTable &b)
+{
+    if (a.getNumVariables() != b.getNumVariables())
+        return false;
 
-    auto sample = *t.getSamples().begin();
+    auto ait = a.cbegin(), bit = b.cbegin();
+    for (; ait != a.cend() && bit != b.cend(); ait++, bit++)
+    {
+        for (unsigned int i = 0; i < a.getNumVariables(); i++)
+        {
+//            std::cout << std::setprecision(SAVE_DOUBLE_PRECISION) << ait->getX().at(i) << " == " << std::setprecision(SAVE_DOUBLE_PRECISION) << bit->getX().at(i) << " ";
+            if (!equalsWithinRange(ait->getX().at(i), bit->getX().at(i)))
+                return false;
+        }
 
-    std::cout << get_size(sample) << std::endl;
+//            std::cout << std::setprecision(SAVE_DOUBLE_PRECISION) << ait->getY().at(j) << " == " << std::setprecision(SAVE_DOUBLE_PRECISION) << bit->getY().at(j) << " ";
+        if (!equalsWithinRange(ait->getY(), bit->getY()))
+            return false;
+//        std::cout << std::endl;
+    }
+
+//    std::cout << "Finished comparing samples..." << std::endl;
+
+    return ait == a.cend() && bit == b.cend();
+}
+
+bool test()
+{
+    DataTable table;
+
+    auto x = std::vector<double>(4);
+    double y;
+    for (double i = -0.0001; i <= 0.0001; i += 0.000001)
+    {
+        for (double j = -0.01; j <= 0.01; j += 0.001)
+        {
+            for (double k = -0.01; k <= 0.01; k += 0.001)
+            {
+                for (double l = -100000.0; l < 0.0; l += 13720.0)
+                {
+                    x.at(0) = i;
+                    x.at(1) = j;
+                    x.at(2) = k;
+                    x.at(3) = l;
+                    y = i * j;
+                    table.addSample(x, y);
+                }
+            }
+        }
+    }
+
+    std::cout << "Size of serialized table: " << get_size(table) << std::endl;
+    exit(1);
 
     StreamType stream;
-    serialize(t, stream);
+    serialize(table, stream);
+    save_to_file("test.datatable", stream);
 
-    auto it = stream.cbegin();
+    DataTable deserializedTable = deserialize<DataTable>(load_from_file("test.datatable"));
 
-    DataTable deserialized = deserialize<DataTable>(it, stream.cend());
+    return is_identical(table, deserializedTable);
+}
 
-    t.printSamples(std::cout);
-    deserialized.printSamples(std::cout);
+int main() {
+    if(test()) {
+        std::cout << "Equal!" << std::endl;
+    }
 
     return 0;
 }
