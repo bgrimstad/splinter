55f8b421446c3aa4bf8a83ae6861eda79e554990
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-02-16 21:07:26 +0100

    Add compiler options to clang as well as gcc and msvc

--- cmake/c11Support.cmake
+++ cmake/c11Support.cmake
@@ -1,5 +1,13 @@
 # Enable C++11
-if(CMAKE_COMPILER_IS_GNUCXX)
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  # using regular Clang or AppleClang
+  add_definitions(-std=c++11)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  # using GCC
   add_definitions(-std=c++0x)
   add_definitions(-std=c++11)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+  # using Intel C++
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  # using Visual Studio C++
 endif()
