a2cbf118d5e4bb24e7364e7d4ddf7078cbfc19b3
gablank <wenhaug@stud.ntnu.no>
2015-05-02 11:57:07 +0200

    Reworked the MatLab interface

--- include/matlab.h
+++ include/matlab.h
@@ -1,14 +1,35 @@
 #ifndef SPLINTER_MATLAB_H
 #define SPLINTER_MATLAB_H
 
-#define API __declspec(dllexport)
+#define obj_ptr int *
+
+#ifndef API
+# define API __declspec(dllexport)
+#endif
 
 #ifdef __cplusplus
 	extern "C"
 	{
 #endif
-		int API *init();
-		double API eval(int *spline, double x);
+		/* DataTable interface */
+		/* Constructor */
+		API obj_ptr datatable_init();
+
+		API void datatable_add_sample(obj_ptr datatable_ptr, double x, double y);
+
+		API void datatable_delete(obj_ptr datatable_ptr);
+
+		/* BSpline interface */
+		// TODO: Make sure MatLab provides the correct type for the BSplineType enum
+		/* Constructor */
+		API obj_ptr bspline_init(obj_ptr datatable_ptr, int type);
+
+		API double bspline_eval(obj_ptr bspline_ptr, double x);
+
+		API void bspline_delete(obj_ptr bspline_ptr);
+
+		/*int API *init();
+		double API eval(int *spline, double x);*/
 #ifdef __cplusplus
 	}
 #endif
--- src/matlab.cpp
+++ src/matlab.cpp
@@ -4,19 +4,89 @@
 
 using namespace Splinter;
 
-	double f(double x)
+	/*double f(double x)
 	{
 		return (4 - 2.1*x*x
 			+ (1 / 3.)*x*x*x*x)*x*x
 			+ x;
-	}
+	}*/
 
+// TODO: #ifdef probably isn't needed here because we will always compile this file
+// with a C++11 compiler. Make sure it is correct, though.
 #ifdef __cplusplus
 	extern "C"
 	{
 #endif
+		/* DataTable interface */
+		/* Cast the obj_ptr to a DataTable * */
+		static DataTable *get_datatable(obj_ptr datatable_ptr) {
+			return (DataTable *)datatable_ptr;
+		}
 
-		int *init() {
+		/* Constructor */
+		obj_ptr datatable_init() {
+			return (obj_ptr) new DataTable();
+		}
+
+		void datatable_add_sample(obj_ptr datatable_ptr, double x, double y) {
+			DataTable *dataTable = get_datatable(datatable_ptr);
+
+			dataTable->addSample(x, y);
+		}
+
+		void datatable_delete(obj_ptr datatable_ptr) {
+			DataTable *dataTable = get_datatable(datatable_ptr);
+
+			delete dataTable;
+		}
+
+		/* BSpline interface */
+		/* Cast the obj_ptr to a BSpline * */
+		static BSpline *get_bspline(obj_ptr datatable_ptr) {
+			return (BSpline *)datatable_ptr;
+		}
+
+		// TODO: Make sure MatLab provides the correct type for the BSplineType enum
+		/* Constructor */
+		obj_ptr bspline_init(obj_ptr datatable_ptr, int type) {
+			DataTable *table = get_datatable(datatable_ptr);
+
+			BSplineType bsplineType;
+			switch (type) {
+			case 1: {
+				bsplineType = BSplineType::LINEAR;
+				break;
+			}
+			case 3: {
+				bsplineType = BSplineType::CUBIC_FREE;
+				break;
+			}
+			case 4: {
+				bsplineType = BSplineType::QUADRATIC_FREE;
+				break;
+			}
+			default: {
+				bsplineType = BSplineType::CUBIC_FREE;
+				break;
+			}
+			}
+
+			return (obj_ptr) new BSpline(*table, bsplineType);
+		}
+
+		double bspline_eval(obj_ptr bspline_ptr, double x) {
+			BSpline *bspline = get_bspline(bspline_ptr);
+
+			return bspline->eval(x);
+		}
+
+		void bspline_delete(obj_ptr bspline_ptr) {
+			BSpline *bspline = get_bspline(bspline_ptr);
+
+			delete bspline;
+		}
+
+		/*int *init() {
 			DataTable samples;
 			double y;
 			double x;
@@ -35,7 +105,7 @@ using namespace Splinter;
 		double eval(int *bspline, double x) {
 
 			return ((BSpline *)bspline)->eval(x);
-		}
+		}*/
 #ifdef __cplusplus
 	}
 #endif
