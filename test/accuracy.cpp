01f66d5e3ab5835334c61034979b38a5e833e09c
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-07-10 16:47:25 +0200

    Added preliminary test for BSplines over all test functions

--- test/accuracy.cpp
+++ test/accuracy.cpp
@@ -7,4 +7,39 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-#include "accuracy.h"
+#include <test_functions.h>
+#include <Catch.h>
+#include <testingutilities.h>
+
+using namespace SPLINTER;
+
+TEST_CASE("BSpline accuracy test", "[bspline]") {
+//    Var x(0);
+//    Var y(1);
+//    Var z(2);
+//    auto f = z * ((x^2) + (y^2));
+//
+//    auto dim = 3;
+//
+//    auto fewSamplePoints = linspace(dim, -5, 5, 7);
+//    auto manySamplePoints = linspace(dim, -5, 5, 10);
+//    auto evalPoints = linspace(dim, -4.95, 4.95, 11);
+//
+//    TestFunction exact(dim, f);
+//    DataTable fewSamples = sample(exact, fewSamplePoints);
+//    DataTable manySamples = sample(exact, manySamplePoints);
+
+    for(auto &exact : testFunctions) {
+        auto dim = exact->getNumVariables();
+        auto samplePoints = linspace(dim, -5, 5, 10);
+        auto evalPoints = linspace(dim, -4.95, 4.95, 11);
+
+        DataTable table = sample(*exact, samplePoints);
+
+        BSpline b(table, BSplineType::CUBIC);
+
+        INFO("Function: " << *(exact->getF()));
+
+        compareFunctions(*exact, b, evalPoints);
+    }
+}
