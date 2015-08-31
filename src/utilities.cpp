c83959cccb0958c301d06697467ed4a93a5e0749
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-08-31 14:40:48 +0200

    Fix result vector of conversion having twice as many rows as it should

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -42,8 +42,6 @@ std::vector<std::vector<double>> denseMatrixToVectorVector(const DenseMatrix &ma
 
     for(size_t i = 0; i < mat.rows(); ++i)
     {
-        vec.push_back(std::vector<double>());
-
         for(size_t j = 0; j < mat.cols(); ++j)
         {
             vec.at(i).push_back(mat(i, j));
