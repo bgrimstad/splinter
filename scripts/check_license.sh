1e95bdaec9c5a56ab0c6153ecd13b6866b78b881
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-09-09 00:17:41 +0200

    Add Python folder to folders to check for license

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -19,7 +19,7 @@ NO_COLOR="\033[0m"
 
 NUM_MISSING=0
 NUM_OK=0
-TO_CHECK="$SPLINTER_DIR/CMakeLists.txt $SPLINTER_DIR/src $SPLINTER_DIR/include $SPLINTER_DIR/matlab $SPLINTER_DIR/test $SPLINTER_DIR/scripts"
+TO_CHECK="$SPLINTER_DIR/CMakeLists.txt $SPLINTER_DIR/src $SPLINTER_DIR/include $SPLINTER_DIR/matlab $SPLINTER_DIR/test $SPLINTER_DIR/scripts $SPLINTER_DIR/python"
 
 function print_red {
 	echo -e "$RED_COLOR$1$NO_COLOR"
