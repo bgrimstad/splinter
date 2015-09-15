6a300c1c81fa2da078b97855a79dfb27e5ac4a63
Anders Wenhaug <wenhaug@stud.ntnu.no>
2015-09-15 11:27:25 +0200

    Renamed matlab.* to cinterface.*

--- src/cinterface.cpp
+++ src/cinterface.cpp
@@ -7,7 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-#include "matlab.h"
+#include "cinterface.h"
 #include <datatable.h>
 #include <bspline.h>
 #include <bsplineapproximant.h>
