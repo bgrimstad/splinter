903d111f1fc1ebfad91da6361178488efdfff18a
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2015-09-26 17:46:51 -0300

    Update to comment in example3.m

--- matlab/example3.m
+++ matlab/example3.m
@@ -5,7 +5,7 @@
 % License, v. 2.0. If a copy of the MPL was not distributed with this
 % file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-% Polynomial regression of noisy sine function
+% B-spline regression of noisy sine function
 close all
 
 % Noisy sine function
