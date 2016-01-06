b91800e58ab336bf08b4abc5bbbecd42024592d6
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2016-01-06 08:46:33 +0100

    Removed precondition matrix for radial basis function networks. A better precondition matrix is needed.

--- src/rbfapproximant.cpp
+++ src/rbfapproximant.cpp
@@ -73,11 +73,9 @@ RBFApproximant::RBFApproximant(const DataTable &samples, RBFType type, bool norm
      * where w is the vector of weights.
      * NOTE: the system is dense and by default badly conditioned.
      * It should be solved by a specialized solver such as GMRES
-     * with preconditioning (e.g. ACBF) as in matlab.
+     * with preconditioning (e.g. ACBF) as in Matlab.
      * NOTE: Consider trying the Łukaszyk–Karmowski metric (for two variables)
      */
-    //SparseMatrix A(numSamples,numSamples);
-    //A.reserve(numSamples*numSamples);
     DenseMatrix A; A.setZero(numSamples, numSamples);
     DenseMatrix b; b.setZero(numSamples,1);
 
@@ -102,8 +100,6 @@ RBFApproximant::RBFApproximant(const DataTable &samples, RBFType type, bool norm
         else b(i) = y;
     }
 
-    //A.makeCompressed();
-
     if (precondition)
     {
         // Calcualte precondition matrix P
@@ -117,9 +113,9 @@ RBFApproximant::RBFApproximant(const DataTable &samples, RBFType type, bool norm
         b = bp;
     }
 
-#ifndef NDEBUG
+    #ifndef NDEBUG
     std::cout << "Computing RBF weights using dense solver." << std::endl;
-#endif // NDEBUG
+    #endif // NDEBUG
 
     // SVD analysis
     Eigen::JacobiSVD<DenseMatrix> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
@@ -128,19 +124,19 @@ RBFApproximant::RBFApproximant(const DataTable &samples, RBFType type, bool norm
     double svalmin = svals(svals.rows()-1);
     double rcondnum = (svalmax <= 0.0 || svalmin <= 0.0) ? 0.0 : svalmin/svalmax;
 
-#ifndef NDEBUG
+    #ifndef NDEBUG
     std::cout << "The reciprocal of the condition number is: " << rcondnum << std::endl;
     std::cout << "Largest/smallest singular value: " << svalmax << " / " << svalmin << std::endl;
-#endif // NDEBUG
+    #endif // NDEBUG
 
     // Solve for weights
     weights = svd.solve(b);
 
-#ifndef NDEBUG
+    #ifndef NDEBUG
     // Compute error. If it is used later on, move this statement above the NDEBUG
     double err = (A*weights - b).norm() / b.norm();
     std::cout << "Error: " << std::setprecision(10) << err << std::endl;
-#endif // NDEBUG
+    #endif // NDEBUG
 
 //    // Alternative solver
 //    DenseQR s;
@@ -161,7 +157,7 @@ double RBFApproximant::eval(DenseVector x) const
 double RBFApproximant::eval(std::vector<double> x) const
 {
     if (x.size() != numVariables)
-        throw Exception("RBFApproximant::eval: Wrong dimension of evaluation point x.");
+        throw Exception("RBFApproximant::eval: Wrong dimension on evaluation point x.");
 
     double fval, sum = 0, sumw = 0;
     int i = 0;
@@ -225,99 +221,11 @@ DenseMatrix RBFApproximant::evalJacobian(DenseVector x) const
 
 /*
  * Calculate precondition matrix
+ * TODO: implement
  */
 DenseMatrix RBFApproximant::computePreconditionMatrix() const
 {
-    DenseMatrix P;
-    P.setZero(numSamples,numSamples);
-
-    // Calculate precondition matrix P based on
-    // purely local approximate cardinal basis functions (ACBF)
-    int sigma = std::max(1.0, std::floor(0.1*numSamples)); // Local points to consider
-
-    int i=0;
-    for (auto it1 = samples.cbegin(); it1 != samples.cend(); ++it1, ++i)
-    {
-        Point p1(it1->getX());
-
-        // Shift data using p1 as origin
-        std::vector<Point> shifted_points;
-        int j=0;
-        for (auto it2 = samples.cbegin(); it2 != samples.cend(); ++it2, ++j)
-        {
-            Point p2(it2->getX());
-            Point p3(p2-p1);
-            p3.setIndex(j); // store index with point
-            shifted_points.push_back(p3);
-        }
-        std::sort(shifted_points.begin(), shifted_points.end());
-
-        // Find sigma closest points to p1
-        std::vector<Point> points;
-        std::vector<int> indices;
-        for (int j=0; j<sigma; j++)
-        {
-            Point p(shifted_points.at(j));
-            indices.push_back(p.getIndex());
-            Point p2 = p+p1;
-            p2.setIndex(p.getIndex());
-            points.push_back(p2); // The resulting point has a different index than that of p
-//                cout << p.getIndex() << "/" << p1.getIndex() << "/" << p2.getIndex() << endl;
-//                assert(p.getIndex() == p2.getIndex());
-        }
-
-        // Add some points far away from p1 and preferably scattered around the domain boundary
-        for (int k=0; k<1; k++)
-        {
-            Point p(shifted_points.at(shifted_points.size()-1-k));
-            indices.push_back(p.getIndex());
-            Point p2 = p+p1;
-            p2.setIndex(p.getIndex());
-            points.push_back(p2);
-        }
-
-        // Build and solve linear system
-        int m = points.size();
-
-        DenseMatrix e; e.setZero(m,1); e(0,0) = 1;
-        DenseMatrix B; B.setZero(m,m);
-        DenseMatrix w; w.setZero(m,1);
-
-        if (points.front().getIndex() != i)
-            throw Exception("RBFApproximant::computePreconditionMatrix: Point has wrong index.");
-
-        for (int k1=0; k1<m; k1++)
-        {
-            for (int k2=0; k2<m; k2++)
-            {
-                Point p = points.at(k1) - points.at(k2);
-                B(k1,k2) = fn->eval(p.dist());
-            }
-        }
-//            DenseQR s;
-//            bool success = s.solve(B,e,w);
-//            assert(success);
-
-        Eigen::JacobiSVD<DenseMatrix> svd(B, Eigen::ComputeThinU | Eigen::ComputeThinV);
-        w = svd.solve(e);
-        //assert(svd.info() == Eigen::Success);
-
-        for (unsigned int j=0; j<numSamples; j++)
-        {
-            auto it = find(indices.begin(),indices.end(),j);
-            if (it!=indices.end())
-            {
-                int k = it - indices.begin();
-
-                P(i,j) = w(k,0);
-                //cout << "j/k/g " << j << "/" << k << "/" << points.at(k).getIndex() << endl;
-                if (points.at(k).getIndex() != j)
-                    throw Exception("RBFApproximant::computePreconditionMatrix: Point has wrong index.");
-            }
-        }
-    }
-
-    return P;
+    return DenseMatrix::Zero(numSamples, numSamples);
 }
 
 /*
@@ -381,7 +289,6 @@ const std::string RBFApproximant::getDescription() const {
     case RBFType::THIN_PLATE_SPLINE:
         description.append("Thin plate spline");
         break;
-
     default:
         description.append("Error: Unknown!");
         break;
