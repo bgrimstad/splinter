f2f46beda1416f9e7e581a04e1427a403c4cbe0e
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2016-03-25 16:08:08 +0100

    Bumped development branch to version 3.1

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='splinter',
-      version='3.0',
+      version='3.1',
       description='A library for multivariate function approximation using splines',
       url='http://github.com/bgrimstad/splinter',
       author='Bjarne Grimstad and others',
