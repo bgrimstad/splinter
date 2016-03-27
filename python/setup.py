08bafca16e8b9f24c1552ec42050e4407702016f
Anders Wenhaug <awenhaug@gmail.com>
2016-03-27 15:05:06 +0200

    Change PyPI project name, append .dev0 to PyPI version

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='splinter',
-      version='3.1',
+setup(name='splinter-py',
+      version='3.1.dev0',
       description='A library for multivariate function approximation using splines',
       url='http://github.com/bgrimstad/splinter',
       author='Bjarne Grimstad and others',
