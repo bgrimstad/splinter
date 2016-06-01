0ab7a19b8094dc52cfb3dc2a3e75e5fabbca81be
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2016-06-01 17:17:40 +0200

    Added unit test for bsplinebasis1d

--- test/unit/bsplinebasis1d.cpp
+++ test/unit/bsplinebasis1d.cpp
@@ -25,7 +25,7 @@ TEST_CASE("supportHack" COMMON_TEXT, COMMON_TAGS)
     REQUIRE(x < 4);
 }
 
-TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
+TEST_CASE("indexSupportedBasisFunctions" COMMON_TEXT, COMMON_TAGS)
 {
     std::vector<double> knots1 = {1, 1, 1, 2.1, 2.5, 2.5, 2.8, 2.8, 2.8, 3.1, 4, 4, 4};
     BSplineBasis1D bb1(knots1, 2);
@@ -33,7 +33,7 @@ TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
     {
         double x = 1;
         std::vector<int> ref = {0, 1, 2};
-        auto sup = bb1.indexSupportedBasisfunctions(x);
+        auto sup = bb1.indexSupportedBasisFunctions(x);
 
         for (unsigned int i = 0; i < ref.size(); ++i)
             REQUIRE(ref.at(i) == sup.at(i));
@@ -42,7 +42,7 @@ TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
     {
         double x = 2.5;
         std::vector<int> ref = {3, 4, 5};
-        auto sup = bb1.indexSupportedBasisfunctions(x);
+        auto sup = bb1.indexSupportedBasisFunctions(x);
 
         for (unsigned int i = 0; i < ref.size(); ++i)
             REQUIRE(ref.at(i) == sup.at(i));
@@ -51,7 +51,7 @@ TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
     {
         double x = 3.999;
         std::vector<int> ref = {7, 8, 9};
-        auto sup = bb1.indexSupportedBasisfunctions(x);
+        auto sup = bb1.indexSupportedBasisFunctions(x);
 
         for (unsigned int i = 0; i < ref.size(); ++i)
             REQUIRE(ref.at(i) == sup.at(i));
@@ -63,7 +63,7 @@ TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
     {
         double x = 0;
         std::vector<int> ref = {0};
-        auto sup = bb2.indexSupportedBasisfunctions(x);
+        auto sup = bb2.indexSupportedBasisFunctions(x);
 
         for (unsigned int i = 0; i < ref.size(); ++i)
             REQUIRE(ref.at(i) == sup.at(i));
@@ -73,7 +73,7 @@ TEST_CASE("indexSupportedBasisfunctions" COMMON_TEXT, COMMON_TAGS)
     {
         double x = 2.499999999;
         std::vector<int> ref = {1, 2};
-        auto sup = bb2.indexSupportedBasisfunctions(x);
+        auto sup = bb2.indexSupportedBasisFunctions(x);
 
         for (unsigned int i = 0; i < ref.size(); ++i)
             REQUIRE(ref.at(i) == sup.at(i));
