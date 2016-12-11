9dcd028993f8dd4685be7982776b8aab61801a69
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2016-12-11 14:00:50 +0100

    Renamed a few more files to be consistent

--- include/bspline_basis_1d.h
+++ include/bspline_basis_1d.h
@@ -82,7 +82,7 @@ private:
     // Builds basis matrix for alternative evaluation of basis functions
     SparseMatrix buildBasisMatrix(double x, unsigned int u, unsigned int k, bool diff = false) const;
 
-    /*
+    /**
      * Builds knot insertion matrix
      * Implements Oslo Algorithm 1 from Lyche and Moerken (2011). Spline methods draft.
      */
