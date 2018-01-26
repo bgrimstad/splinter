96b5fac971ccca13e69606634dbb3bfbe3539f23
Bjarne Grimstad <bjarne.grimstad@gmail.com>
2018-01-26 16:50:55 +0100

    Add some comments to builders

--- src/bspline_builder.cpp
+++ src/bspline_builder.cpp
@@ -50,6 +50,12 @@ BSpline BSpline::Builder::fit(const DataTable &data, Smoothing smoothing, double
     return BSpline(_dim_x, _dim_y, knotVectors, _degrees).fit(data, smoothing, alpha, weights);
 }
 
+/**
+ * Create a B-spline that interpolates the sample points
+ * @param data A table of sample points on a regular grid
+ * @param degree The degree of the B-spline basis functions
+ * @return A B-spline that interpolates the sample points
+ */
 BSpline bspline_interpolator(const DataTable &data, unsigned int degree)
 {
     auto dim_x = data.get_dim_x();
@@ -61,11 +67,25 @@ BSpline bspline_interpolator(const DataTable &data, unsigned int degree)
     return BSpline(dim_x, dim_y, knot_vectors, degrees).fit(data);
 }
 
+/**
+ * Create a cubic B-spline that interpolates the sample points
+ * In the multivariate case, the multivariate basis functions will be products of univariate cubic basis functions,
+ * i.e. for two variables the basis functions are bi-cubic, etc.
+ * @param data A table of sample points on a regular grid
+ * @return A cubic B-spline that interpolates the sample points
+ */
 BSpline cubic_bspline_interpolator(const DataTable &data)
 {
     return bspline_interpolator(data, 3);
 }
 
+/**
+ * Create a P-spline (penalized B-spline) that smooths the sample points
+ * @param data A table of sample points on a regular grid
+ * @param degree The degree of the B-spline basis functions
+ * @param alpha Smoothing/regularization factor
+ * @return A B-spline that smooths the sample points (P-spline)
+ */
 BSpline pspline_approximator(const DataTable &data, unsigned int degree, double alpha)
 {
     auto dim_x = data.get_dim_x();
@@ -77,6 +97,14 @@ BSpline pspline_approximator(const DataTable &data, unsigned int degree, double 
     return BSpline(dim_x, dim_y, knot_vectors, degrees).fit(data, BSpline::Smoothing::PSPLINE, alpha);
 }
 
+/**
+ * Create a cubic P-spline (penalized B-spline) that smooths the sample points.
+ * In the multivariate case, the multivariate basis functions will be products of univariate cubic basis functions,
+ * i.e. for two variables the basis functions are bi-cubic, etc.
+ * @param data A table of sample points on a regular grid
+ * @param alpha Smoothing/regularization factor
+ * @return A cubic B-spline that smooths the sample points (cubic P-spline)
+ */
 BSpline cubic_pspline_approximator(const DataTable &data, double alpha)
 {
     return pspline_approximator(data, 3, alpha);
