1113eb124d355d5f239f0f135a19a9b32e9f57d5
Anders Wenhaug <awenhaug@gmail.com>
2018-11-17 20:22:36 +0100

    Tell Pickle how to handle the BSpline class

--- python/splinterpy/__init__.py
+++ python/splinterpy/__init__.py
@@ -5,28 +5,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-from .datatable import DataTable
-from .bspline import BSpline
-from .bsplinebuilders import bspline_interpolator, bspline_smoother, bspline_unfitted
-from .bsplineboosting import BSplineBoosting
+import copyreg
+import tempfile
 
 from .splinter_backend import splinter_backend_obj
 
-try:
-    splinter_backend_obj.load()
-except Exception as e:
-    print(e)
-
-__version__ = splinter_backend_obj.__version__
-
-
-__all__ = [
-    "splinter_backend",
-    "bspline",
-    "bsplinebuilders",
-    "bsplineboosting"
-]
+from .bspline import BSpline
+from .datatable import DataTable
+from .bsplineboosting import BSplineBoosting
+from .bsplinebuilders import bspline_interpolator, bspline_smoother, bspline_unfitted
 
 
 def load(lib_file_path: str):
@@ -37,3 +24,39 @@ def load(lib_file_path: str):
     :return:
     """
     splinter_backend_obj.load(lib_file_path)
+
+
+# Tell Pickle how to pickle and unpickle the BSpline class
+def constructor(serialized_data) -> BSpline:
+    with tempfile.NamedTemporaryFile() as temp:
+        with open(temp.name, "wb") as f:
+            f.write(serialized_data)
+
+        return BSpline.from_json(temp.name)
+
+
+def reducer(bspline: BSpline):
+    with tempfile.NamedTemporaryFile() as temp:
+        bspline.to_json(temp.name)
+        with open(temp.name, "rb") as f:
+            data = f.read()
+
+    return constructor, (data, )
+
+
+# Register reducer as the reducer for objects of type BSpline
+copyreg.pickle(BSpline, reducer)
+
+__version__ = splinter_backend_obj.__version__
+
+__all__ = [
+    "splinter_backend",
+    "bspline",
+    "bsplinebuilders",
+    "bsplineboosting"
+]
+
+try:
+    splinter_backend_obj.load()
+except Exception as e:
+    print(e)
