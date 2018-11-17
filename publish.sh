427dcb61dffbe1ee31f5a14c1e939d9e0336f0e2
Anders Wenhaug <awenhaug@gmail.com>
2018-11-17 20:14:03 +0100

    Add license info to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 #
+# This file is part of the SPLINTER library.
+# Copyright (C) 2012 Bjarne Grimstad (bjarne.grimstad@gmail.com).
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # Publish SPLINTER to (Test)PyPI
 # Publish to TestPyPI: ./publish.sh
 # Publish to PyPI: ./publish.sh release
-#
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BUILD_DIR="${DIR}/build/"
